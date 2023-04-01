f42934df20b7b85dc84b8bbc2360d7f7d3972bb0
eivid <eivid@DESKTOP-SJAPMOT>
2023-04-02 02:50:11 +0300

    Adding schedule desing

--- src/screens/ScheduleView.js
+++ src/screens/ScheduleView.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, StatusBar, Image } from 'react-native'
+import { View, Text, SafeAreaView, StatusBar, Image, Alert, ScrollView } from 'react-native'
 import React from 'react'
 import { StyleSheet } from 'react-native';
 import {
@@ -10,77 +10,146 @@ import BottomBar from '../components/BottomBar';
 
 const HomeView = () => {
 
+
     //Čia turi buti gaunama info iš fire base apie kalendoriu
+    const lecture = [
+        {
+            name: 'Kompiuteriu architektura',
+            lecture: 2,
+            hall: 'SRL-24A4',
+            group: 1,
+        },
+        {
+            name: 'Operacinės sistemos',
+            lecture: 3,
+            hall: 'SRL-24A4',
+            group: 0,
+        },
+        {
+            name: 'Lietuvių kalba',
+            lecture: 6,
+            hall: 'SRL-24A4',
+            group: 1,
+        }
+    ]
 
-    // const lectures = [
-    //     {
-    //         name: 'Kompiuteriu architektura',
-    //         lecture: 2,
-    //         hall: 'SRL-24A4',
-    //         group: 1,
-    //     },
-    //     {
-    //         name: 'Lietuviu kalba',
-    //         lecture: 3,
-    //         hall: 'SRL-24A4',
-    //         group: 0,
-    //     }
-    // ]
+    const weekDays = [
+        {
+            weekDay: 'Monday',
+            lectures: lecture
+        },
+        {
+            weekDay: 'Tuesday',
+            lectures: lecture
+        },
+        {
+            weekDay: 'Wenesday',
+            lectures: lecture
+        },
+        {
+            weekDay: 'Thursday',
+            lectures: lecture
+        },
+        {
+            weekDay: 'Friday',
+            lectures: lecture
+        },
+    ]
 
-    // const weekDay = [
-    //     {
-    //         weekDay: 'Monday',
-    //         lectures: []
-    //     },
-    //     {
-    //         weekDay: 'Tuesday',
-    //         lectures: []
-    //     },
-    // ]
+    //Reikia pagal tvarkarašti nustatyti kuri sav bus atvaizduojama 
+    const currentWeek = 1
 
-    // weekDay[0] = lectures
-    // weekDay[1] = lectures
+    // weekDays[0].lectures = lecture
+    // weekDays[1].lectures = lecture
 
-    // var calendarView = []
+    var scheduleHeader = []
+    scheduleHeader.push(
+        <View style={styles.calendarItem}>
 
-    // for (let i = 0; i < weekDay.length; i++) {
+            <View style={[styles.calendarColumn, { flex: 1 }]}>
+                <Text style={styles.calendarText}>No.</Text>
+            </View>
 
-    //     // for(let i = 0; i < weekDay[i].lectures.length; i++)
-    //     // {
+            <View style={[styles.calendarColumn, { flex: 4 }]}>
+                <Text style={styles.calendarText}>Lecture</Text>
+            </View>
 
-    //     // }
+            <View style={[styles.calendarColumn, { flex: 3 }]}>
+                <Text style={styles.calendarText}>Room</Text>
+            </View>
 
-    //     //Čia duomenis taip perparsinti ko reikia
+            <View style={[styles.calendarColumn, { flex: 1 }]}>
+                <Text style={styles.calendarText}>SG</Text>
+            </View>
+        </View>
+    )
 
+    var calendarViews = []
 
-    //     calendarView.push(
-    //         <View key={i} style={styles.calendarElement}>
-    //             <Text style={[{ fontWeight: '700', fontSize: 25, }]}>{calendar[i].weekDay}</Text>
-
-    //             <View>
-                    
-    //             </View>
-
-
-    //         </View>
+    // weekDays.forEach(weekDay=> 
     //     )
-    //}
+
+    for (let i = 0; i < weekDays.length; i++) {
+        var lectures = weekDays[i].lectures;
+
+        var lecturesView = [];
+
+        for (let i = 0; i < lectures.length; i++) {
+
+            var rowBackground = {
+                backgroundColor: '#dde2e7`'
+            }
+
+            if (i % 2 == 0) {
+                rowBackground.backgroundColor = '#D3D4D5'
+            }
+
+            lecturesView.push(
+                <View style={[styles.calendarItem, rowBackground]}>
+
+                    <View style={[styles.calendarColumn, { flex: 1 }]}>
+                        <Text style={styles.calendarText}>{lectures[i].lecture}</Text>
+                    </View>
+
+                    <View style={[styles.calendarColumn, { flex: 4 }]}>
+                        <Text style={styles.calendarText}>{lectures[i].name}</Text>
+                    </View>
+
+                    <View style={[styles.calendarColumn, { flex: 3 }]}>
+                        <Text style={styles.calendarText}>{lectures[i].hall}</Text>
+                    </View>
+
+                    <View style={[styles.calendarColumn, { flex: 1 }]}>
+                        <Text style={styles.calendarText}>{lectures[i].group}</Text>
+                    </View>
+                </View>
+            )
+        }
+
+        calendarViews.push(
+            <View key={i} style={styles.calendarElement}>
+                <Text style={[{ fontWeight: '700', fontSize: 25, }]}>{weekDays[i].weekDay}</Text>
+                {scheduleHeader}
+                {lecturesView}
+            </View>
+        )
+    }
 
     return (
         <View style={{ flex: 1, backgroundColor: '#DFE5EB' }}>
             <View style={styles.main}>
-                <SafeAreaView  >
 
-                    <View style={styles.weekStyle}>
-                        <View style={styles.stuff}>
-                            <Text style={{ fontWeight: '700', fontSize: 20, color: 'white' }}>Week 1</Text>
-                        </View>
+                <View style={styles.weekStyle}>
+                    <View style={styles.stuff}>
+                        <Text style={{ fontWeight: '700', fontSize: 20, color: 'white' }}>Week {currentWeek}</Text>
                     </View>
+                </View>
 
+                <ScrollView  >
                     <View style={styles.calendarContainer}>
-                        {/* {calendarView} */}
+                        {calendarViews}
                     </View>
-                </SafeAreaView>
+                </ScrollView>
             </View>
             <BottomBar></BottomBar>
         </View>
@@ -115,6 +184,7 @@ const styles = StyleSheet.create({
     },
     weekStyle: {
         marginTop: 8,
+        marginBottom: 8,
         justifyContent: 'center',
         alignItems: 'center'
     },
@@ -126,6 +196,18 @@ const styles = StyleSheet.create({
         borderRadius: 50,
         width: '30%',
         backgroundColor: '#7E7B80'
+    },
+    calendarItem: {
+        // backgroundColor: 'red',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    calendarColumn: {
+        borderWidth: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 40
+        //width: ''
     }
 });
 
