a017b11c3ccf5c98d4b6474cc6fb2c597ded5cbe
Eividas Balciunas <xeive7x@gmail.com>
2023-04-24 16:28:57 +0300

    initial commit

---
+++ Portal/src/firebase/database.js
@@ -0,0 +1,19 @@
+import firebase from '@react-native-firebase/app';
+import '@react-native-firebase/firestore';
+
+const firebaseConfig = {
+  appId: '1:305836683096:android:529325e5bc48a51ba47f2d',
+  projectId: 'vgtu-app-9a58d',
+  name: 'VGTU-App',
+  apiKey: 'AIzaSyC_8U4ay_9PWymZZgUVv1UQUEDgx1r7kD0',
+  databaseURL: '',
+  messagingSenderId: '',
+  storageBucket: '',
+};
+
+console.log(firebase.apps.length)
+if (firebase.apps.length == 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
